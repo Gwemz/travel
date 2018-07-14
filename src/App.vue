89374c67823582b54ed74a08023c31486de75978
Gwemz <3396543978@qq.com>
2018-07-14 18:49:41 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- https://gitee.com/dell-lee/Travel -->
+    <!-- <img src="./assets/logo.png"> -->
     <router-view/>
   </div>
 </template>
